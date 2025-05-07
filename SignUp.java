023f8e4d9749b32f5256f8a137105547f0f7e89e
Mariya Byehan <maria_byehan@yahoo.com>
2025-05-07 14:59:56 +0200

    last changes

--- SignUp.java
+++ SignUp.java
@@ -6,25 +6,25 @@ public class SignUp {
         //Los datos que debes pedir al usuario son:
         //1. Nombre
         System.out.println("Cuál es tu nombre?");
-        String nombre = scanner.nextLine();
+        String name = scanner.nextLine();
         System.out.println();
         //2. Apellido
         System.out.println("Cuál es tu apellido?");
-        String apellido = scanner.nextLine();
+        String lastName = scanner.nextLine();
         System.out.println();
         //3. Nombre de usuario
         System.out.println("Elige tu nombre de usuario");
-        String usuario = scanner.nextLine();
+        String user = scanner.nextLine();
         System.out.println();
         //4. Contraseña
         System.out.println("Elige tu contraseña");
-        String contraseña = scanner.nextLine();
+        String password = scanner.nextLine();
         System.out.println();
         //Añade una nueva línea antes de mostrar la siguiente pregunta.
         //Imprimir el siguiente resultado:
         //Hola <nombre> <apellido>, tu nombre de usuario es <usuario> y tu contraseña es <contraseña>, gracias por registrarte.
         //Añade una nueva línea antes de mostrar la respuesta
-        System.out.println("Hola " + nombre + " " + apellido + ", tu nombre de usuario es " + usuario + " y tu contraseña es " + contraseña + ", gracias por registrarte.");
+        System.out.println("Hola " + name + " " + lastName + ", tu nombre de usuario es " + user + " y tu contraseña es " + password + ", gracias por registrarte.");
         scanner.close();
     }
 }
